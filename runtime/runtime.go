8461c5be41316eb2251f46f38068cfc01921fd13
iqinning <q383175101@163.com>
2019-07-15 06:43:40 +0800

    优化

--- runtime/runtime.go
+++ runtime/runtime.go
@@ -5,28 +5,12 @@ import (
 	"time"
 )
 
-func init() {}
-
 type IRuntime interface {
 	Start(cmd string, args ...string) int
 	Stop()
-	ReStart(cmd string, args ...string) int
 	Idle() int
 	Health() int
 	GetCreateTime() time.Time
 	Init()
 	event.IEventEmitter
-}
-
-type Runtime struct {
-	event.EventEmitter
-	CreateTime time.Time
-}
-
-func (runtime *Runtime) GetCreateTime() time.Time {
-	return runtime.CreateTime
-}
-
-func (runtime *Runtime) Init() {
-	runtime.CreateTime = time.Now()
 }
